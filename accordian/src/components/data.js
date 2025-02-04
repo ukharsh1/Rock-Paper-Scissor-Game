f835b7284feee7949392090b052e77c9424a8092
harsh Uke <harshuke14@gmail.com>
2025-02-04 13:28:41 +0530

    https://github.com/ukharsh1/Rock-Paper-Scissor-Game.git

---
+++ accordian/src/components/data.js
@@ -0,0 +1,8 @@
+const data = [
+    { id: 1, question: "What is your favorite programming language?", answer: "JavaScript" },
+    { id: 2, question: "What inspires your creativity?", answer: "Nature and art" },
+    { id: 3, question: "What is your go-to debugging technique?", answer: "Using console logs and breakpoints" },
+    { id: 4, question: "How do you prioritize tasks in a project?", answer: "By impact and urgency" },
+    { id: 5, question: "What is the most interesting dataset you've worked with?", answer: "Customer behavioral trends dataset" }
+  ];
+   
---
+++ accordian/src/components/index.jsx
@@ -0,0 +1,7 @@
+export default function Accordian(){
+    return (
+        <div >
+       Accordian
+        </div>
+    );
+}
